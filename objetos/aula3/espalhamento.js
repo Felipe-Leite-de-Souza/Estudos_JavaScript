dc328b4b3118c35d55961d5ddfc4da7cfc4878d3
fleitedeso <felipe_leitesouza@hotmail.com>
2023-03-29 23:24:51 -0300

    percorrendo objetos

---
+++ objetos/aula3/espalhamento.js
@@ -0,0 +1,33 @@
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["11987654321", "11123456789"],
+};
+
+// Comentar para forçar o erro
+cliente.enderecos = [
+    {
+        rua: "R. Koseph Climber",
+        numero: 1337,
+        apartamento: true,
+        complemento: "ap 934",
+    }
+];
+
+function ligaParaCliente(telefoneComercial, telefoneResidencial) {
+    console.log(`Ligando para ${telefoneComercial}.`);
+    console.log(`Ligando para ${telefoneResidencial}.`);
+}
+
+// ligaParaCliente(cliente.telefone[0], cliente.telefone[1]);
+
+// Espalhamento
+ligaParaCliente(...cliente.telefone);
+
+const encomenda = {
+    destinatario: cliente.nome,
+    ...cliente.enderecos[0],
+};
+
+console.log(encomenda);
