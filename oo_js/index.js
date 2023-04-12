6e9c2935554d59a1729a4a79af7359f3ffc0a91a
fleitedeso <felipe_leitesouza@hotmail.com>
2023-04-12 13:35:59 -0300

    getters

--- oo_js/index.js
+++ oo_js/index.js
@@ -2,9 +2,8 @@ import User from "./User.js";
 import Docente from "./Docente.js"
 import Admin from "./Admin.js"
 
-const novoUser = new User("Mariana", "m@m.com", "1990-01-01");
-console.log(novoUser.exibirInfos());
+// const novoUser = new User("Mariana", "m@m.com", "1990-01-01");
+// console.log(novoUser.exibirInfos());
 
 const novoAdmin = new Admin("Rodrigo", "r@r.com", "1983-01-01");
-console.log(novoAdmin.exibirInfos());
-console.log(novoAdmin.nomeAdmin());
+console.log(novoAdmin.nome);
