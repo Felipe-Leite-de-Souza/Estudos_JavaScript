90b9b0a67b6a03b24d5e50e55785056042060e0e
fleitedeso <felipe_leitesouza@hotmail.com>
2023-04-12 14:01:52 -0300

    polimorfismo

--- oo_js/index.js
+++ oo_js/index.js
@@ -5,7 +5,10 @@ import Admin from "./Admin.js"
 // const novoUser = new User("Mariana", "m@m.com", "1990-01-01");
 // console.log(novoUser.exibirInfos());
 
-const novoAdmin = new Admin("Rodrigo", "r@r.com", "1983-01-01");
-console.log(novoAdmin.nome);
-novoAdmin.nome = "";
-console.log(novoAdmin.nome);
+// const novoAdmin = new Admin("Rodrigo", "r@r.com", "1983-01-01");
+// console.log(novoAdmin.nome);
+// novoAdmin.nome = ""; // forçando um erro
+// console.log(novoAdmin.nome);
+
+const novoDocente = new Docente("Guilherme", "g@g.com", "1978-01-01");
+console.log(novoDocente.exibirInfos());
